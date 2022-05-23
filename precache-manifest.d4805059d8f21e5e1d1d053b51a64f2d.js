3f11b4528abdb588b2612f29341d40956db91ad7
anand <raghubanshi15anand@gmail.com>
2022-05-23 13:33:03 -0700

    Updates

---
+++ precache-manifest.d4805059d8f21e5e1d1d053b51a64f2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6336c618bada13c9adf66410d010f987",
+    "url": "/newreactwebsite/index.html"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/newreactwebsite/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "5548f20f352ea3275437",
+    "url": "/newreactwebsite/static/css/main.9b76c41c.chunk.css"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/newreactwebsite/static/js/2.5c9724e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/newreactwebsite/static/js/2.5c9724e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5548f20f352ea3275437",
+    "url": "/newreactwebsite/static/js/main.bb6363e0.chunk.js"
+  },
+  {
+    "revision": "51e6a24c9a46e61bac58",
+    "url": "/newreactwebsite/static/js/runtime-main.26c506b8.js"
+  }
+]);
